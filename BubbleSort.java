d87c37f8d1ed9975bef1818e247b1fc8ba67c441
itzsravani <61891020+itzsravani@users.noreply.github.com>
2020-08-26 15:16:00 -0700

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -11,15 +11,16 @@ public class BubbleSort {
 	}
 
 	private static void bubblesort(int[] array) {
-		int temp=0;
+		//int temp=0;
 		
 		for( int i=0;i<array.length;i++){
-			for(int j=i;j< array.length-i;j++){
-				if(array[j]> array[j+i] ) {
-					temp = array[j];
-					array[j] =array[j+1];
-					array[j+1]=temp;
-				}
+			for (int j = 0; j < array.length-1-i; j++) { 
+			     if(array[j]>array[j+1])
+			     {
+			       int temp=array[j];
+			       array[j]=array[j+1];
+			       array[j+1]=temp;
+			     }
 			}
 		}
 		System.out.println("After sorting:"+Arrays.toString(array));
